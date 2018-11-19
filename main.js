37600ffef0e6ad3beefb9092623d3a22af3401be
arliber <leonliber@gmail.com>
2018-11-19 18:17:18 +0200

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  let startTimestamp, stopTimestamp;
+
+  initSensors();
+
+  $('#startSession').on('click', function() {
+    $('#recording').fadeIn();
+    startTimestamp = (new Date()).getTime();
+
+    startRecording();
+    startSensorRecording();
+
+    /*setTimeout(function(){
+      $('#stopSession').trigger('click');
+    }, 3000);*/
+
+  });
+
+  $('#stopSession').on('click', function () {
+    $('#recording').fadeOut();
+    $('#audioNote').hide();
+    stopTimestamp = (new Date()).getTime();
+
+    stopRecording(startTimestamp, stopTimestamp);
+    stopSensorRecording();
+    downloadSensorData(startTimestamp, stopTimestamp);
+
+    log({
+      message: 'Finished ' + (stopTimestamp - startTimestamp) / 1000 + ' seconds session with ' + sessionData.length +' data points',
+      type: 'success'
+    });
+
+  });
+
+});
